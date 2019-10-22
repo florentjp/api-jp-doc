72c3deec45605bb9245d98efd0ecf7b04fcaf335
Mathis Guilbon <mathis.guilbon@epitech.eu>
2019-10-22 14:08:01 +0200

    pre-merge

--- apiDoc/siteConfig.js
+++ apiDoc/siteConfig.js
@@ -106,7 +106,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/florentjp/api-jp-doc',
+  //repoUrl: 'https://github.com/florentjp/api-jp-doc',
 };
 
 module.exports = siteConfig;
